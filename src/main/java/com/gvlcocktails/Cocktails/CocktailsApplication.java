a24c45e46bf9829189b4323b1db3f6d7a35edbff
chocopirate <vlcek.gabriel@gmail.com>
2020-06-14 16:19:29 +0200

    Spring Initializr

---
+++ src/main/java/com/gvlcocktails/Cocktails/CocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.gvlcocktails.Cocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvlcocktails/Cocktails/CocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvlcocktails.Cocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
